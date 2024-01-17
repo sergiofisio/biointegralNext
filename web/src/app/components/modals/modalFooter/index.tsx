22b6edab7666bfff03b21860ab8abc14eaf8756d
Sergio Augusto Moreira Bastos Junior <sergiobastosfisio@yahoo.com.br>
2024-01-17 12:44:36 -0300

    modal

--- web/src/app/components/modals/modalFooter/index.tsx
+++ web/src/app/components/modals/modalFooter/index.tsx
@@ -1,12 +1,13 @@
-export default function ModalFooter({ children }: any) {
-    return <div *ngIf="showModal"
-    className="fixed right-0 top-0 z-10 w-full h-full flex items-center justify-center bg-blue bg-opacity-50">
-    <div className="bg-white px-4 pt-5 w-1/3 h-4/6 pb-4 rounded-3xl relative flex flex-col items-center justify-center">
-      <h2 className="text-3xl font-bold">Fale Conosco</h2>
-      <h2
+export default function ModalFooter() {
+  return (
+    <div className="fixed right-0 top-0 z-10 w-full h-full flex items-center justify-center bg-blue bg-opacity-50">
+      <div className="bg-white px-4 pt-5 w-1/3 h-4/6 pb-4 rounded-3xl relative flex flex-col items-center justify-center">
+        <h2 className="text-3xl font-bold">Fale Conosco</h2>
+        {/* <h2
         className="text-xl absolute right-2 top-2 text-white bg-blue rounded-[100%] w-5 h-5 flex items-center justify-center cursor-pointer"
         (click)="handleModal($event)">X</h2>
-      <app-form className="w-full" />
+      <app-form className="w-full" /> */}
+      </div>
     </div>
-  </div>
+  );
 }
