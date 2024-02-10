0316f2232dcb6e48baed63f4f7d53c228a33951c
Sergio Bastos <sergiobastosfisio@gmail.com>
2024-02-10 10:51:35 -0300

    novo formulario e botão

--- src/app/components/button/index.tsx
+++ src/app/components/button/index.tsx
@@ -1,23 +1,32 @@
+import { tailwindProperty } from "./../../functions/tailwind";
+import { PulseLoader } from "react-spinners";
 export default function Button({
-  text,
-  className,
-  onClick,
-  disabled,
+	text,
+	className,
+	onClick,
+	disabled,
 }: {
-  text: string;
-  className?: string;
-  onClick: (e: any) => void;
-  disabled?: boolean;
+	text: string;
+	className?: string;
+	onClick: (e: any) => void;
+	disabled?: boolean;
 }) {
-  return (
-    <button
-      className={`${className} h-20 text-white font-poppins rounded-3xl text-2xl p-4 transition-all duration-500 ease-in-out border-2 border-solid md:text-xl`}
-      onClick={onClick}
-      disabled={disabled}
-    >
-      {text === "Voltar" && "⋘ "}
-      {text}
-      {text === "Próximo" && " ⋙"}
-    </button>
-  );
+	return (
+		<button
+			className={`${className} h-20 text-white font-poppins rounded-3xl text-2xl p-4 transition-all duration-500 ease-in-out border-2  border-solid md:text-xl ${
+				disabled
+					? "cursor-not-allowed bg-gray-500 border-gray-500"
+					: `cursor-pointer bg-blue ${tailwindProperty("hover", [
+							"text-blue",
+							"bg-white",
+					  ])}`
+			}`}
+			onClick={onClick}
+			disabled={disabled}
+		>
+			{text === "Voltar" && !disabled && "⋘ "}
+			{disabled ? <PulseLoader color='white' /> : text}
+			{text === "Próximo" && !disabled && " ⋙"}
+		</button>
+	);
 }
--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -2,62 +2,62 @@ import Button from "../button";
 import Image from "next/image";
 
 const socials = [
-  {
-    name: "Facebook",
-    icon: "/assets/site/logo_social/fb.svg",
-    url: "https://www.facebook.com/saudebiointegral/",
-  },
-  {
-    name: "Instagram",
-    icon: "/assets/site/logo_social/instagram.svg",
-    url: "https://www.instagram.com/biointegralsaude/",
-  },
+	{
+		name: "Facebook",
+		icon: "/assets/site/logo_social/fb.svg",
+		url: "https://www.facebook.com/saudebiointegral/",
+	},
+	{
+		name: "Instagram",
+		icon: "/assets/site/logo_social/instagram.svg",
+		url: "https://www.instagram.com/biointegralsaude/",
+	},
 ];
 
 export default function Footer({ setShowModal }: { setShowModal: any }) {
-  return (
-    <footer id="contato" className="flex flex-col items-start w-full bg-white">
-      <Image
-        className="w-80 h-auto md:w-60 md:h-auto"
-        src="/assets/site/LOGO.svg"
-        alt="Logo"
-        width={200}
-        height={200}
-      />
-      <div className="w-full bg-blue h-full text-white text-center py-6 px-10 gap-10 flex flex-col items-center">
-        <h2 className="text-6xl">Dúvidas?</h2>
-        <h2 className="text-3xl">
-          Tire suas dúvidas com nossos especialistas. Agende sua consulta em uma
-          de nossas unidades ou envie sua mensagem pelo formulário abaixo.
-        </h2>
-        <Button
-          className="w-52 h-20 text-white font-poppins bg-blue rounded-3xl text-2xl p-4 transition-all duration-500 ease-in-out border-2 border-solid hover:text-blue hover:bg-white"
-          text="Contato"
-          onClick={() => setShowModal({ type: "contato" })}
-        />
+	return (
+		<footer id='contato' className='flex flex-col items-start w-full bg-white'>
+			<Image
+				className='h-auto w-80 md:w-60 md:h-auto'
+				src='/assets/site/LOGO.svg'
+				alt='Logo'
+				width={200}
+				height={200}
+			/>
+			<div className='flex flex-col items-center w-full h-full gap-10 px-10 py-6 text-center text-white bg-blue'>
+				<h2 className='text-6xl'>Dúvidas?</h2>
+				<h2 className='text-3xl'>
+					Tire suas dúvidas com nossos especialistas. Agende sua consulta em uma
+					de nossas unidades ou envie sua mensagem pelo formulário abaixo.
+				</h2>
+				<Button
+					className='border-white w-52'
+					text='Contato'
+					onClick={() => setShowModal({ type: "contato" })}
+				/>
 
-        <div className="flex w-full justify-center">
-          <div className="w-full h-fit flex justify-center items-center">
-            {socials.map((social, key) => {
-              return (
-                <a
-                  className="flex items-center justify-center w-40 h-auto"
-                  key={key}
-                  href={social.url}
-                >
-                  <Image
-                    className="flex w-20 h-20"
-                    width={200}
-                    height={200}
-                    src={social.icon}
-                    alt={`logo ${social.name}`}
-                  />
-                </a>
-              );
-            })}
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
+				<div className='flex justify-center w-full'>
+					<div className='flex items-center justify-center w-full h-fit'>
+						{socials.map((social, key) => {
+							return (
+								<a
+									className='flex items-center justify-center w-40 h-auto'
+									key={key}
+									href={social.url}
+								>
+									<Image
+										className='flex w-20 h-20'
+										width={200}
+										height={200}
+										src={social.icon}
+										alt={`logo ${social.name}`}
+									/>
+								</a>
+							);
+						})}
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
 }
