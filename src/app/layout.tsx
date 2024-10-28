42cf10d499ecb585c0b39edd049fe448f6138111
Sergio Bastos Jr <sergiobastosfisio@gmail.com>
2024-10-28 09:50:48 -0300

    clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
+import Script from "next/script";
 import type { Metadata as NextMetadata } from "next";
 import { Toaster } from "sonner";
-
+import Head from "next/head";
 import "./globals.css";
 
 interface Metadata extends NextMetadata {
@@ -18,7 +19,7 @@ export const metadata: Metadata = {
     "Dr. Sergio e Dra Fresia - Pioneiros no Brasil de várias técnicas como MICROFISIOTERAPIA, PSYCH-K® e BIODECODAGE",
   favicon: "/favicon.ico",
   keywords:
-    "Biointegral Saúde, MICROFISIOTERAPIA, PSYCH-K®, BIODECODAGE, saúde integral, terapias complementares, bem-estar, saúde emocional, saúde física",
+    "biointegral saúde, microfisioterapia, psych, psych-k®, biodecodage, saúde integral, terapias complementares, bem-estar, saúde emocional, saúde física",
   metadataBase: new URL("https://www.biointegral.com.br"),
   openGraph: {
     title: "Biointegral Saúde",
@@ -32,6 +33,30 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-br">
+      <Head>
+        <title>{String(metadata.title)}</title>
+        <meta name="description" content={String(metadata.description)} />
+        <meta name="keywords" content={String(metadata.keywords)} />
+        <meta property="og:title" content={String(metadata.openGraph?.title)} />
+        <meta
+          property="og:description"
+          content={String(metadata.openGraph?.description)}
+        />
+        <meta
+          property="og:url"
+          content={String(metadata.openGraph?.url?.toString())}
+        />
+        <meta
+          property="og:image"
+          content={
+            Array.isArray(metadata.openGraph?.images) &&
+            metadata.openGraph.images.length > 0
+              ? metadata.openGraph.images[0].toString()
+              : "/assets/OG/default.png"
+          }
+        />
+        <link rel="icon" href={metadata.favicon || "/favicon.ico"} />
+      </Head>
       <body
         className="max-w-[100vw] h-screen relative"
         suppressHydrationWarning={true}
@@ -47,6 +72,17 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             classNames: {
               closeButton: `!bg-black !text-white !right-2 !left-none !top-2 !w-5 !h-5 hover:!bg-white hover:!text-black`,
             },
+          }}
+        />
+        <Script
+          id="clarity-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "opomyyzey5");`,
           }}
         />
       </body>
