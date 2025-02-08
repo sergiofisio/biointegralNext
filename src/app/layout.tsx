8f1558ed640da89bcccb15574742f80255a5d469
Sergio Bastos <sergiobastosfisio@gmail.com>
2025-02-08 15:12:08 -0300

    new menu hamburguer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,8 @@
-import type { Metadata as NextMetadata } from "next";
+import type { Metadata } from "next";
 import { Toaster } from "sonner";
-import Head from "next/head";
 import "./globals.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import Script from "next/script";
-
-interface Metadata extends NextMetadata {
-  favicon?: string;
-}
 
 export const viewport = {
   width: "device-width",
@@ -18,46 +13,41 @@ export const metadata: Metadata = {
   title: "Biointegral Saúde",
   description:
     "Dr. Sergio e Dra Fresia - Pioneiros no Brasil de várias técnicas como MICROFISIOTERAPIA, PSYCH-K® e BIODECODAGE",
-  favicon: "/favicon.ico",
-  keywords:
-    "biointegral saúde, microfisioterapia, psych, psych-k®, biodecodage, saúde integral, terapias complementares, bem-estar, saúde emocional, saúde física",
+  keywords: [
+    "biointegral saúde",
+    "microfisioterapia",
+    "psych",
+    "psych-k®",
+    "biodecodage",
+    "saúde integral",
+    "terapias complementares",
+    "bem-estar",
+    "saúde emocional",
+    "saúde física",
+  ],
   metadataBase: new URL("https://www.biointegralsaude.com.br"),
   openGraph: {
     title: "Biointegral Saúde",
     description:
       "Dr. Sergio e Dra Fresia - Pioneiros no Brasil de várias técnicas como MICROFISIOTERAPIA, PSYCH-K® e BIODECODAGE",
-    url: new URL("https://www.biointegralsaude.com.br"),
-    images: ["/assets/OG/site.png"],
+    url: "https://www.biointegralsaude.com.br",
+    images: [
+      {
+        url: "/assets/OG/site.png",
+        width: 1200,
+        height: 630,
+        alt: "Biointegral Saúde",
+      },
+    ],
+  },
+  icons: {
+    icon: "/favicon.ico",
   },
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-br">
-      <Head>
-        <title>{String(metadata.title)}</title>
-        <meta name="description" content={String(metadata.description)} />
-        <meta name="keywords" content={String(metadata.keywords)} />
-        <meta property="og:title" content={String(metadata.openGraph?.title)} />
-        <meta
-          property="og:description"
-          content={String(metadata.openGraph?.description)}
-        />
-        <meta
-          property="og:url"
-          content={String(metadata.openGraph?.url?.toString())}
-        />
-        <meta
-          property="og:image"
-          content={
-            Array.isArray(metadata.openGraph?.images) &&
-            metadata.openGraph.images.length > 0
-              ? metadata.openGraph.images[0].toString()
-              : "/assets/OG/site.png"
-          }
-        />
-        <link rel="icon" href={metadata.favicon || "/favicon.ico"} />
-      </Head>
       <body
         className="max-w-[100vw] h-screen relative"
         suppressHydrationWarning={true}
