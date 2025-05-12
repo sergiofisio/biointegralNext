4b0ce72f319c8f113ffec92ad3100ba09e765a0f
Sergio Bastos <sergiobastosfisio@gmail.com>
2025-05-12 13:24:26 -0300

    deploy hostger

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
   swcMinify: true,
   compiler: {
     styledComponents: true,
+  },
+  images: {
+    unoptimized: true,
   },
 };
 
@@ -33,7 +37,7 @@ module.exports = withSentryConfig(module.exports, {
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
   // side errors will fail.
-  tunnelRoute: "/monitoring",
+  // tunnelRoute: "/monitoring",
 
   // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
