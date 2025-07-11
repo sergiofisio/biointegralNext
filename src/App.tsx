67a551ca1ce91f8b9067bb81e7a5ddb4245fef7d
Sergio Bastos <sergiobastosfisio@gmail.com>
2025-07-11 10:16:44 -0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-// import { Route, Routes, useLocation } from "react-router-dom";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
 import Home from "./pages/page";
 import Satisfacao from "./pages/satisfacao/page";
-// import HeaderForm from "./components/header/form";
-// import Header from "./components/header";
+import HeaderForm from "./components/header/form";
+import Header from "./components/header";
 
 export default function App() {
-  // const location = useLocation();
+  const location = useLocation();
   return (
     <>
-      {/* {location.pathname !== "/" ? <HeaderForm /> : <Header />} */}
+      {location.pathname !== "/" ? <HeaderForm /> : <Header />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/satisfacao" element={<Satisfacao />} />
